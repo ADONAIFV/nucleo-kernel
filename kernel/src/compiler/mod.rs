230ec864257a4649783a000c463726a666e32a22
ADONAIFV <166330966+ADONAIFV@users.noreply.github.com>
2026-06-22 22:06:55 -0400

    Eliminar kernel/src/compiler/mod.rs

--- kernel/src/compiler/mod.rs
+++
@@ -1,6 +0,0 @@
-//! Compilador de unikernels para agentes especializados.
-//! Permite compilar un agente en su propio sistema operativo.
-
-pub mod unikernel;
-
-pub use unikernel::UnikernelCompiler;
